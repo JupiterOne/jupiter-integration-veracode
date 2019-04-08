d151e34adff7eebe2564c9f28338a4d7ad70758e
Isaac Williams <isaac.williams@lifeomic.com>
2019-04-08 12:32:18 -0400

    JPT-1420: Always create dynamic & static services

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+import { ServiceEntityMap } from "./types";
+
 export const VERACODE_ACCOUNT_ENTITY_TYPE = "veracode_account";
 export const VERACODE_SERVICE_ENTITY_TYPE = "veracode_scan";
 export const VERACODE_CWE_ENTITY_TYPE = "cwe";
@@ -11,3 +13,21 @@ export const VERACODE_VULNERABILITY_CWE_RELATIONSHIP_TYPE =
   "veracode_finding_exploits_cwe";
 export const VERACODE_VULNERABILITY_FINDING_RELATIONSHIP_TYPE =
   "veracode_finding_is_vulnerability";
+export const DEFAULT_SERVICE_ENTITY_MAP: ServiceEntityMap = {
+  DYNAMIC: {
+    _class: "Service",
+    _key: "veracode-scan-dynamic",
+    _type: VERACODE_SERVICE_ENTITY_TYPE,
+    category: "software",
+    displayName: "DYNAMIC",
+    name: "DYNAMIC",
+  },
+  STATIC: {
+    _class: "Service",
+    _key: "veracode-scan-static",
+    _type: VERACODE_SERVICE_ENTITY_TYPE,
+    category: "software",
+    displayName: "STATIC",
+    name: "STATIC",
+  },
+};
