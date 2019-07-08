31b66a22b4be55312f66c9b2fa99f9950a1a5543
Isaac Williams <isaac.williams@lifeomic.com>
2019-07-08 14:54:14 -0400

    Add basic test for vulnerability entity conversion

--- src/converters.test.ts
+++ src/converters.test.ts
@@ -1,57 +1,64 @@
-import { ApplicationData, FindingData, toFindingEntity } from "./converters";
+import {
+  ApplicationData,
+  FindingData,
+  toFindingEntity,
+  toVulnerabilityEntity,
+} from "./converters";
 
-test("convert findings -> finding entities and date type properties -> number type", () => {
-  const finding: FindingData = {
-    cvss: 1,
-    cwe: {
-      id: "id",
-      name: "name",
-      description: "description",
-      references: [
-        {
-          name: "name",
-          url: "url",
-        },
-      ],
-      recommendation: "recommendation",
-      remediation_effort: 111,
-      severity: 111,
-    },
+const findingData: FindingData = {
+  cvss: 1,
+  cwe: {
+    id: "id",
+    name: "name",
     description: "description",
-    exploitability: 111,
-    finding_status: {
-      "111": {
-        status: "status",
-        reopened: true,
-        resolution: "resolution",
-        resolution_status: "resolution_status",
-        found_date: "2019-04-22T21:43:53.000Z",
-        resolved_date: "2019-04-22T21:43:53.000Z",
-        modified_date: "2019-04-22T21:43:53.000Z",
-        finding_source: {
-          module: "module",
-          file_name: "file_name",
-          file_line_number: "file_line_number",
-          file_path: "file_path",
-        },
+    references: [
+      {
+        name: "name",
+        url: "url",
+      },
+    ],
+    recommendation: "recommendation",
+    remediation_effort: 1,
+    severity: 1,
+  },
+  description: "description",
+  exploitability: -1,
+  finding_status: {
+    "111": {
+      status: "status",
+      reopened: true,
+      resolution: "resolution",
+      resolution_status: "resolution_status",
+      found_date: "2019-04-22T21:43:53.000Z",
+      resolved_date: "2019-04-22T21:43:53.000Z",
+      modified_date: "2019-04-22T21:43:53.000Z",
+      finding_source: {
+        module: "module",
+        file_name: "file_name",
+        file_line_number: "file_line_number",
+        file_path: "file_path",
       },
     },
-    finding_category: {
-      id: "id",
-      description: "description",
-      name: "name",
-    },
-    guid: "guid",
-    severity: 111,
-    scan_type: "scan_type",
-  };
+  },
+  finding_category: {
+    id: "id",
+    description: "description",
+    name: "name",
+  },
+  guid: "guid",
+  severity: 1,
+  scan_type: "scan_type",
+};
+
+test("convert findings -> finding entities and date type properties -> number type", () => {
   const application: ApplicationData = {
     guid: "111",
     profile: {
       name: "name",
     },
   };
-  expect(toFindingEntity(finding, application)).toEqual({
+
+  expect(toFindingEntity(findingData, application)).toEqual({
     _class: "Finding",
     _key: "veracode-finding-guid",
     _type: "veracode_finding",
@@ -69,5 +76,26 @@ test("convert findings -> finding entities and date type properties -> number ty
     sourceFilePath: "file_path",
     sourceModule: "module",
     targets: "name",
+  });
+});
+
+test("vulnerabilities", () => {
+  expect(toVulnerabilityEntity(findingData)).toEqual({
+    _class: "Vulnerability",
+    _key: "veracode-vulnerability-id",
+    _type: "veracode_vulnerability",
+    category: "application",
+    cvss: 1,
+    cwe: "id",
+    description: "description",
+    displayName: "name",
+    exploitability: "Unlikely",
+    id: "id",
+    name: "name",
+    numericExploitability: -1,
+    numericSeverity: 1,
+    public: false,
+    scanType: "scan_type",
+    severity: "Very Low",
   });
 });
